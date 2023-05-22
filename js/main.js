7b2b82adcdd5baa2693414e7953f243ce5808ce0
mostafa tarek <133722479+mostafa-tarek1@users.noreply.github.com>
2023-05-22 21:20:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//  Responsive Navbar
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
